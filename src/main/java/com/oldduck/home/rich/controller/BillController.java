480d9aa469819989f298bb43f0865053050c56cb
5ive1ne9ine <ex-caihh@cmfchina.com>
2020-11-02 16:40:03 +0800

    cundang

--- src/main/java/com/oldduck/home/rich/controller/BillController.java
+++ src/main/java/com/oldduck/home/rich/controller/BillController.java
@@ -7,9 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
@@ -29,15 +29,15 @@ public class BillController {
     private BillManager billManager;
 
     @RequestMapping("/addBill")
-    public String addBill(@RequestBody BillDetailDto billDetailDto) {
+    public String addBill(@RequestParam BillDetailDto billDetailDto) {
         billManager.addBill(billDetailDto);
-        return "html/index.html";
+        return "index";
     }
 
     @RequestMapping("/delBill")
     public String delBill(@RequestBody String serialno) {
         billManager.deleteBill(serialno);
-        return "html/index.html";
+        return "index";
     }
 
     @RequestMapping("/queryDetail")
